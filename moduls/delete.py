2b8054ad4fe29a111275afcfa59b199db8b6051b
0leShera <ole_shera@mail.ru>
2022-03-24 17:31:41 +0300

    Add password request for deleting a user

--- moduls/delete.py
+++ moduls/delete.py
@@ -1,16 +1,16 @@
 def user_del(email, user_emails, users_storage):
 
     if email in user_emails:
-        choice = input(f"Are you sure you want to delete the user {email}?\nEnter 'yes' or 'not': ").lower()
-        if choice == "yes" or choice == "y":
-            users_storage.pop(email)
-            print(f"User_email {email} deleted.")
+        user_pass = input("Enter password: ")
+        if user_pass == users_storage[email]['password']:
+            choice = input(f"Are you sure you want to delete the user {email}?\nEnter 'yes' or 'not': ").lower()
+            if choice == "yes" or choice == "y":
+                users_storage.pop(email)
+                print(f"User_email {email} deleted.")
+            else:
+                input("Please choice action 'create' or 'read', or 'update', or 'delete': ").lower()
         else:
-            input("Please choice action 'create' or 'read', or 'update', or 'delete': ").lower()
-        # print(f"User_email = {email} deleted.")
-        # users_storage.pop(email)
-
-        # print(f"{users_storage}")
+            print("You entered invalid password. Profile access denied.")
 
     else:
         print(f"No user with email: {email}")
